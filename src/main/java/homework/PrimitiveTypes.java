dcccef8e42f9112cfc36809a63ab39b714e5a74e
alexsian <sijanov@gmail.com>
2022-01-04 20:27:50 +0200

    [feature/java1]: Homework

---
+++ src/main/java/homework/PrimitiveTypes.java
@@ -0,0 +1,261 @@
+package homework;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        // Bytes Data Types
+
+        byte smallestNumberForThisType = -128;
+        byte verySmallNumber = -120;
+        byte SmallNumber = 0;
+        byte anotherSmallNumber = 1;
+        byte numberOfSuitcases = 3;
+        byte wardrobesInTheApartment = 4;
+        byte pencilsITheApartment = 10;
+        byte pensInTheApartment = 15;
+        byte cupsInTheApartment = 25;
+        byte largestNumberForThisType = 127;
+        System.out.println(smallestNumberForThisType);
+        System.out.println(verySmallNumber);
+        System.out.println(SmallNumber);
+        System.out.println(anotherSmallNumber);
+        System.out.println(numberOfSuitcases);
+        System.out.println(wardrobesInTheApartment);
+        System.out.println(pencilsITheApartment);
+        System.out.println(pensInTheApartment);
+        System.out.println(cupsInTheApartment);
+        System.out.println(largestNumberForThisType);
+
+        System.out.println("Byte minimal value:" + " " + Byte.MIN_VALUE);
+        System.out.println("Byte maximal value: " + Byte.MAX_VALUE);
+
+        // Short Data Types
+
+        short wholeDog = 1;
+        short dogHead = 1;
+        short dogNose = 1;
+        short dogTail = 1;
+        short dogMouth = 1;
+        short dogTongue = 1;
+        short dogEars = 2;
+        short dogEyes = 2;
+        short dogPaws = 4;
+        short dogTeeth = 42;
+        System.out.println(wholeDog);
+        System.out.println(dogHead);
+        System.out.println(dogNose);
+        System.out.println(dogTail);
+        System.out.println(dogMouth);
+        System.out.println(dogTongue);
+        System.out.println(dogEars);
+        System.out.println(dogEyes);
+        System.out.println(dogPaws);
+        System.out.println(dogTeeth);
+
+        System.out.println("Short minimal value: " + Short.MIN_VALUE);
+        System.out.println("Short maximal value: " + Short.MAX_VALUE);
+
+        // Integer Data Types
+
+        int republicsInTheSovietUnion = 15;
+        int landGermany = 16;
+        int statesUsa = 50;
+        int statesIndia = 28;
+        int regionsRussia = 46;
+        int municipalitiesLatvia = 494;
+        int BalticStates = 3;
+        int regionsUkraine = 24;
+        int regionsBelarus = 6;
+        int countriesWorld = 126 + 126;
+        System.out.println(republicsInTheSovietUnion);
+        System.out.println(landGermany);
+        System.out.println(statesUsa);
+        System.out.println(statesIndia);
+        System.out.println(regionsRussia);
+        System.out.println(municipalitiesLatvia);
+        System.out.println(BalticStates);
+        System.out.println(regionsUkraine);
+        System.out.println(regionsBelarus);
+        System.out.println(countriesWorld);
+
+        System.out.println("Integer minimal value:" + " " + Integer.MIN_VALUE);
+        System.out.println("Integer minimal value: " + Integer.MAX_VALUE);
+
+        // Long Data Types
+
+        long squareOfRussia = 17125191L;
+        long squareOfLatvia = 64589L;
+        long squareOfUkraine = 603628L;
+        long squareOfBelarus = 207600L;
+        long squareOfBelgium = 30689L;
+        long lithuaniaSquare = 65300L;
+        long EstoniaSquare = 45339L;
+        long squareOfFrance = 543940L;
+        long austriaSquare = 83879L;
+        long landArea = 510100000L;
+
+        System.out.println(squareOfRussia);
+        System.out.println(squareOfLatvia);
+        System.out.println(squareOfUkraine);
+        System.out.println(squareOfBelarus);
+        System.out.println(squareOfBelgium);
+        System.out.println(lithuaniaSquare);
+        System.out.println(EstoniaSquare);
+        System.out.println(squareOfFrance);
+        System.out.println(austriaSquare);
+        System.out.println(landArea);
+
+        System.out.println("long minimal value :" + Long.MIN_VALUE);
+        System.out.println("Long maximal value:" + " " + Long.MAX_VALUE);
+
+        // Float Data Types
+
+        float inchToCentimeter = 2.54F;
+        float footToMeter = 0.3048F;
+        float poundToKg = 2.20462F;
+        float landMileToKm = 1.60934F;
+        float nauticalMileToKm = 1.853F;
+        float dollarToEuro = 0.89F;
+        float czechKronaToEuro = 0.04F;
+        float rubleToEuro = 0.012F;
+        float rubleToDollar = 0.013F;
+        float halfLiter = 1 / 2F;
+
+        System.out.println(inchToCentimeter);
+        System.out.println(footToMeter);
+        System.out.println(poundToKg);
+        System.out.println(landMileToKm);
+        System.out.println(nauticalMileToKm);
+        System.out.println(dollarToEuro);
+        System.out.println(czechKronaToEuro);
+        System.out.println(rubleToEuro);
+        System.out.println(rubleToDollar);
+        System.out.println(halfLiter);
+
+        System.out.println("Float minimal value: " + Float.MIN_VALUE);
+        System.out.println("Float maximal value:" + " " + Float.MAX_VALUE);
+
+        // Double Data Types
+
+        double thePopulationOfLatviaInMillions = 1.893223;
+        double thePopulationOfRussiaInMillions = 146.171015;
+        double thePopulationOfLithuaniaInMillions = 3.0538;
+        double thePopulationOfBelarusInMillions = 9.399;
+        double thePopulationOfBrazilInMillions = 212.6;
+        double thePopulationOfEstoniaInMillions = 1.331;
+        double thePopulationOfAustriaInMillions = 9.045673;
+        double thePopulationOfGermanyInMillions = 83.756658;
+        double thePopulationOfBelgiumInMillions = 11.718929;
+        double thePopulationOfUkraineInMillions = 41.588354;
+
+        System.out.println(thePopulationOfLatviaInMillions);
+        System.out.println(thePopulationOfRussiaInMillions);
+        System.out.println(thePopulationOfLithuaniaInMillions);
+        System.out.println(thePopulationOfBelarusInMillions);
+        System.out.println(thePopulationOfBrazilInMillions);
+        System.out.println(thePopulationOfEstoniaInMillions);
+        System.out.println(thePopulationOfAustriaInMillions);
+        System.out.println(thePopulationOfGermanyInMillions);
+        System.out.println(thePopulationOfBelgiumInMillions);
+        System.out.println(thePopulationOfUkraineInMillions);
+
+        System.out.println("Double minimal value: " + Double.MIN_VALUE);
+        System.out.println("Double maximal value:" + " " + Double.MAX_VALUE);
+
+        // Addition examples
+
+        int a = 15;
+        double b = 3.5;
+        double c = a + b;
+        System.out.println(c);
+
+        int res = 5 + 3;
+        System.out.println(res);
+
+        // Examples for subtraction
+
+        double x = 19.4;
+        float y = 5.3F;
+        double r = x - y;
+        System.out.println(r);
+
+        int g = 140;
+        long l = 120L;
+        long lr = g - l;
+        System.out.println(lr);
+
+
+        // Examples for multiplication
+
+        short s = 456;
+        int i = 23;
+        int sr = s * i;
+        System.out.println(sr);
+
+        byte bt = -4 * 25;
+        System.out.println(bt);
+
+        // Division examples
+
+        int id = 10;
+        double dd = 2.7;
+        double dr = id / dd;
+        System.out.println(dr);
+
+
+        double ir = 5D / 2;
+        double kr = 5.0 / 2;
+        if (ir == kr) {
+            System.out.printf("%.2f = Результат одинаковый = %.2f\n", ir, kr);
+        }
+
+        // Modulo division
+
+        int m = 6 % 5;
+        double mn = 6.4 % 5;
+        System.out.println(m);
+        System.out.println(mn);
+
+        // Character Data Type
+
+        char charV = '\u0432';
+        char charE = 'е';
+        char charD = 0x0414;
+        char charU = 'y';
+        char charL = 'л';
+        char charM = 'м';
+        char tire = 0x002D;
+        char noSuchLetter = 'я';
+        char charR = 'р';
+        char charEe = '\u0435';
+
+        System.out.printf("%s%s%s%s %s %s%s%s%s%s\n", charD, charE, charL, charU, tire, charV, charR, charEe, charM, noSuchLetter);
+
+
+        // String Data Types
+
+        String myCityIs = "Riga";
+        String myCountry = "Latvia";
+        String myPlanet = "Earth";
+        String myDogsNameIs = "Arturo";
+        String TvBrand = "Samsung";
+
+        System.out.println(myCityIs);
+        System.out.println(myCountry);
+        System.out.println(myPlanet);
+        System.out.println(myDogsNameIs);
+        System.out.println(TvBrand);
+
+        // My name is Alexander I am 51 years old I was tortured to invent names of variables!
+
+        String myName = "My name is Alexander";
+        String myOld = "I am 51 years old";
+        String myCondition = "I was tortured";
+        String variableNames = "to invent names of variables!";
+        System.out.println(myName + " " + myOld + " " + myCondition + " " + variableNames);
+        System.out.printf("%s %s %s %s\n", myName, myOld ,myCondition, variableNames);
+
+
+    }
+
+}
