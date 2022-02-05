56c7c4f2ed4620a0c7eecde62489209305bfae16
alexsijan <sijanov@gmail.com>
2022-02-05 19:23:24 +0200

    [master]: CouponTest commit

---
+++ src/test/java/CouponTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CouponTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void initDriver() {
+        driver = new ChromeDriver();
+    }
+
+@Test
+    public void applyCouponCode() {
+        driver.get("https://shop.acodemy.lv");
+ //       driver.getTitle();
+ //   System.out.println(driver.getTitle());
+    assertEquals("Online shop – acodemy – Just another WordPress site", driver.getTitle());
+//   assertThat(driver.getTitle(), containsString("nline shop"));
+    }
+
+
+    @AfterEach
+    public void closePage() {
+        driver.quit();
+    }
+}
